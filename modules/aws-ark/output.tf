8d0246c6f5a05990f5120be5212dde69821f6ed7
Luca Novara <luca@sighup.io>
2019-03-11 18:57:31 +0100

    added ark CRDs generation from Terraform

--- modules/aws-ark/output.tf
+++ modules/aws-ark/output.tf
@@ -4,10 +4,36 @@ locals {
 aws_access_key_id=${aws_iam_access_key.ark-backup.id}
 aws_secret_access_key=${aws_iam_access_key.ark-backup.secret}
 EOF
+
+  ark-crds = <<EOF
+apiVersion: ark.heptio.com/v1
+kind: BackupStorageLocation
+metadata:
+  name: default
+spec:
+  provider: aws
+  objectStorage:
+    bucket: ${aws_s3_bucket.ark-backup.bucket}
+  config:
+    region: ${var.aws_region}
+---
+apiVersion: ark.heptio.com/v1
+kind: VolumeSnapshotLocation
+metadata:
+  name: default
+spec:
+  provider: aws
+  config:
+    region: ${var.aws_region}
+EOF
 }
 
 output "ark-credentials" {
   value = "${local.ark-credentials}"
+}
+
+output "ark-crds" {
+  value = "${local.ark-crds}"
 }
 
 output "bucket_username" {
