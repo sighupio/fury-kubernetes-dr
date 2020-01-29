208dc65bc6f942637b71642a9d909dd4fd5be782
Ángel Barrera Sánchez <angel@sighup.io>
2020-01-29 11:02:08 +0100

    Add basic E2E testing pipeline

---
+++ katalog/tests/velero/velero-backup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bats
+
+load ./../helper
+
+@test "Trigger backup" {
+    info
+    backup() {
+        velero backup create backup-e2e --from-schedule manifests -n kube-system --wait
+    }
+    run backup
+    [ "$status" -eq 0 ]
+}
+
+@test "upps. Chaos...." {
+    info
+    chaos() {
+        kubectl delete service velero -n kube-system
+    }
+    run chaos
+    [ "$status" -eq 0 ]
+}
+
+@test "Restore backup" {
+    info
+    backup() {
+        velero restore create --from-backup backup-e2e -n kube-system --wait
+    }
+    run backup
+    [ "$status" -eq 0 ]
+}
+
+
+@test "Test Recovery" {
+    info
+    test() {
+        kubectl get service velero -n kube-system
+    }
+    run test
+    [ "$status" -eq 0 ]
+}
+
+@test "Delete backup" {
+    info
+    delete(){
+        velero backup delete backup-e2e --confirm -n kube-system
+    }
+    run delete
+    [ "$status" -eq 0 ]
+}
---
+++ katalog/tests/velero/velero-install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bats
+
+load ./../helper
+
+@test "Applying Monitoring" {
+    info
+    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v1.3.0/katalog/prometheus-operator/crd-servicemonitor.yml
+    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v1.3.0/katalog/prometheus-operator/crd-rule.yml
+}
+
+@test "Deploy Velero on Prem" {
+    deploy() {
+        apply katalog/velero-on-prem
+    }
+    run deploy
+    [ "$status" -eq 0 ]
+}
+
+@test "Velero is Running" {
+    info
+    test() {
+        kubectl get pods -l app=velero -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
+    }
+    loop_it test 60 5
+    status=${loop_it_result}
+    [ "$status" -eq 0 ]
+}
+
