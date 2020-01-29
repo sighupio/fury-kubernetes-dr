a064c08c6472c2bcc9cf29e31d072ac1ac2e0a15
Ángel Barrera Sánchez <angel@sighup.io>
2020-01-29 15:33:06 +0100

    Add 1.15 and 1.14 pipelines

--- example/aws-example/main.tf
+++ example/aws-example/main.tf
@@ -6,25 +6,30 @@ variable "region" {
   default = "eu-west-1"
 }
 
+variable "my_cluster_name" {}
+variable "environment" {
+  default = "testing"
+}
+
 module "velero" {
   source             = "../../modules/aws-velero"
-  cluster_name       = "e2e-testing"
-  environment        = "testing"
-  backup_bucket_name = "sighup-e2e-testing-velero"
+  cluster_name       = "${var.my_cluster_name}"
+  environment        = "${var.environment}"
+  backup_bucket_name = "${var.my_cluster_name}-${var.environment}-velero"
   aws_region         = "${var.region}"
 }
 
 output "cloud_credentials" {
-  value = "${module.velero.cloud_credentials}"
+  value     = "${module.velero.cloud_credentials}"
   sensitive = true
 }
 
 output "backup_storage_location" {
-  value = "${module.velero.backup_storage_location}"
+  value     = "${module.velero.backup_storage_location}"
   sensitive = true
 }
 
 output "volume_snapshot_location" {
-  value = "${module.velero.volume_snapshot_location}"
+  value     = "${module.velero.volume_snapshot_location}"
   sensitive = true
 }
