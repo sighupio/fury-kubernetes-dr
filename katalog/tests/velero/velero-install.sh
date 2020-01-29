4c139d391e8871b649141cf25d04d9de9e6f07f9
Ángel Barrera Sánchez <angel@sighup.io>
2020-01-29 14:45:36 +0100

    FIX: Test name

--- katalog/tests/velero/velero-install.sh
+++ katalog/tests/velero/velero-install.sh
@@ -21,7 +21,7 @@ load ./../helper
     test() {
         kubectl get pods -l app=velero -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
     }
-    loop_it test 60 5
+    loop_it test 60 10
     status=${loop_it_result}
     [ "$status" -eq 0 ]
 }
@@ -34,7 +34,7 @@ load ./../helper
     [ "$status" -eq 0 ]
 }
 
-@test "Velero is Running" {
+@test "Velero Restic is Running" {
     info
     test() {
         kubectl get pods -l app=velero-restic -o json -n kube-system |jq '.items[].status.containerStatuses[].ready' | uniq | grep -q true
