5f61767a3d369bf12cee991cc35307cd385a646c
Ángel Barrera Sánchez <angel@sighup.io>
2020-09-22 14:19:59 +0200

    Add license header in eks module

--- modules/eks-velero/iam.tf
+++ modules/eks-velero/iam.tf
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2020 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 resource "aws_iam_policy" "velero_backup" {
   name = "${var.name}-${var.env}-velero-backup"
 
