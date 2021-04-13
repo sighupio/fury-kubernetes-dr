f69bf2557d999e2eddbcd2ec51c4bedb10db086a
Niccolo Raspa <niccolo@sighup.io>
2021-04-13 17:59:15 +0200

    Add workload identity

--- modules/gcp-velero/iam.tf
+++ modules/gcp-velero/iam.tf
@@ -4,26 +4,8 @@
  * license that can be found in the LICENSE file.
  */
 
-resource "random_id" "velero" {
-  byte_length = 2
-}
-
-resource "google_service_account" "velero" {
-  project      = var.project
-  account_id   = "velero-sa-${random_id.velero.hex}"
-  display_name = "Velero account for ${var.backup_bucket_name}"
-}
-
-resource "google_service_account_key" "velero" {
-  service_account_id = google_service_account.velero.name
-}
-
-# Custom role 
-resource "google_project_iam_custom_role" "velero-role" {
-  role_id     = "velero_role_${random_id.velero.hex}"
-  title       = "Velero Role"
-  description = "Custom role to assign to velero sa to allow it to create snapshots"
-  permissions = [
+locals {
+  velero_role_permissions = [
     "compute.disks.get",
     "compute.disks.list",
     "compute.disks.create",
@@ -37,9 +19,35 @@ resource "google_project_iam_custom_role" "velero-role" {
   ]
 }
 
+resource "google_service_account" "velero" {
+  project      = var.project
+  account_id   = var.google_service_account_name
+  display_name = "Service account for Velero"
+}
+
+resource "google_service_account_key" "velero" {
+  service_account_id = google_service_account.velero.name
+}
+
+resource "google_project_iam_custom_role" "velero-role" {
+  role_id     = "${replace(var.google_service_account_name, "-", "_")}_role"
+  title       = "Velero Role"
+  description = "Custom role to assign to velero sa to allow it to create snapshots"
+  permissions = var.workload_identity ? concat(local.velero_role_permissions, ["iam.serviceAccounts.signBlob"]) : local.velero_role_permissions
+}
+
 resource "google_project_iam_member" "project" {
   project = var.project
   role    = google_project_iam_custom_role.velero-role.id
   member  = "serviceAccount:${google_service_account.velero.email}"
 }
 
+# Workload identity
+resource "google_service_account_iam_binding" "roles" {
+  count = var.workload_identity ? 1 : 0
+  members = [
+    "serviceAccount:niccolo-raspa-test-project.svc.id.goog[kube-system/${var.workload_identity_kubernetes_service_account}]",
+  ]
+  role               = "roles/iam.workloadIdentityUser"
+  service_account_id = google_service_account.velero.id
+}
