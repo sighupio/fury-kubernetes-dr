8cfd8b905c1b5638f95053ed5de31b71a9d9fb75
Niccolo Raspa <niccolo@sighup.io>
2021-04-13 16:51:53 +0200

    Add custom role to velero service account

--- modules/gcp-velero/iam.tf
+++ modules/gcp-velero/iam.tf
@@ -4,12 +4,42 @@
  * license that can be found in the LICENSE file.
  */
 
+resource "random_id" "velero" {
+  byte_length = 2
+}
+
 resource "google_service_account" "velero" {
   project      = var.project
-  account_id   = "${var.backup_bucket_name}-velero"
+  account_id   = "velero-sa-${random_id.velero.hex}"
   display_name = "Velero account for ${var.backup_bucket_name}"
 }
 
 resource "google_service_account_key" "velero" {
   service_account_id = google_service_account.velero.name
 }
+
+# Custom role 
+resource "google_project_iam_custom_role" "velero-role" {
+  role_id     = "velero_role_${random_id.velero.hex}"
+  title       = "Velero Role"
+  description = "Custom role to assign to velero sa to allow it to create snapshots"
+  permissions = [
+    "compute.disks.get",
+    "compute.disks.list",
+    "compute.disks.create",
+    "compute.disks.createSnapshot",
+    "compute.snapshots.get",
+    "compute.snapshots.list",
+    "compute.snapshots.create",
+    "compute.snapshots.useReadOnly",
+    "compute.snapshots.delete",
+    "compute.zones.get"
+  ]
+}
+
+resource "google_project_iam_member" "project" {
+  project = var.project
+  role    = google_project_iam_custom_role.velero-role.id
+  member  = "serviceAccount:${google_service_account.velero.email}"
+}
+
