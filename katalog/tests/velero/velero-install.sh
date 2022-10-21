937f705ccc1c61fc7f894f07a5e31beeb0ea3992
Luca Novara <luca@sighup.io>
2022-10-21 16:42:30 +0200

    fix(ci): fixed Prometheus Operator CRDs links

--- katalog/tests/velero/velero-install.sh
+++ katalog/tests/velero/velero-install.sh
@@ -9,8 +9,8 @@ load ./../helper
 
 @test "Applying Monitoring" {
     info
-    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v1.13.0-rc/katalog/prometheus-operator/crd-servicemonitor.yml
-    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v1.13.0-rc/katalog/prometheus-operator/crd-rule.yml
+    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v2.0.0/katalog/prometheus-operator/crds/0prometheusruleCustomResourceDefinition.yaml
+    kubectl apply -f https://raw.githubusercontent.com/sighupio/fury-kubernetes-monitoring/v2.0.0/katalog/prometheus-operator/crds/0servicemonitorCustomResourceDefinition.yaml
 }
 
 @test "Deploy Velero on Prem" {
