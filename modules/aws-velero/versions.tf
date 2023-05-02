227dd0acb07669ed16fb4322207fbaf7fd35b8ad
Giuseppe Iannelli <94362884+g-iannelli@users.noreply.github.com>
2023-05-02 17:25:15 +0200

    chore(module,aws-velero): change required terraform version to >=1.3.0

--- modules/aws-velero/versions.tf
+++ modules/aws-velero/versions.tf
@@ -5,8 +5,11 @@
  */
 
 terraform {
-  required_version = ">= 0.15.4"
+  required_version = ">= 1.3.0"
   required_providers {
-    aws = ">= 3.37.0"
+    aws = {
+      version = "~> 3.37.0"
+      source  = "hashicorp/aws"
+    }
   }
 }
