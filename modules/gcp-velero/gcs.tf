4e512e1937fcc4f0f8e7f6853cd442ac0453a431
Filo01 <git@filippo.sh>
2025-03-21 09:11:17 +0100

    fix: gcp wait for service account

--- modules/gcp-velero/gcs.tf
+++ modules/gcp-velero/gcs.tf
@@ -18,6 +18,19 @@ resource "google_storage_bucket" "main" {
   labels = var.tags
 }
 
+resource "null_resource" "verify_service_account" {
+  depends_on = [google_service_account.velero]
+  
+  provisioner "local-exec" {
+    command = <<EOT
+      until gcloud iam service-accounts describe ${google_service_account.velero.email} --format="value(email)" 2>/dev/null; do
+        echo "Waiting for service account to be fully created..."
+        sleep 5
+      done
+    EOT
+  }
+}
+
 resource "google_storage_bucket_iam_binding" "velero_bucket_iam" {
   bucket = google_storage_bucket.main.name
   role   = "roles/storage.objectAdmin"
@@ -26,10 +39,7 @@ resource "google_storage_bucket_iam_binding" "velero_bucket_iam" {
     "serviceAccount:${google_service_account.velero.email}"
   ]
 
-  depends_on = [
-    google_service_account.velero,
-    google_service_account_key.velero
-  ]
+  depends_on = [null_resource.verify_service_account]
 
   lifecycle {
     prevent_destroy = false
